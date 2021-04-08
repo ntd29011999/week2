3f1f24ea69ab8c85312c42c7f9752dea6bf5bb07
Hung Le <tinbo12345@gmail.com>
2021-04-08 21:41:27 +0700

    them Task 2 + Task 3

--- week2/app/src/main/java/com/example/week1/LoginActivity.kt
+++ week2/app/src/main/java/com/example/week1/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.week1
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,19 +8,27 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-class SigninActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        var btnLogin = findViewById<Button>(R.id.LoginButton)
+
         val et_email_login=findViewById<EditText>(R.id.editTextTextPersonName2);
         val et_pass_login=findViewById<EditText>(R.id.editTextTextPassword);
-
+        val btnLogin = findViewById<Button>(R.id.LoginButton)
         btnLogin.setOnClickListener {
             if(et_email_login.text.toString()=="ronaldo@gmail.com"&&et_pass_login.text.toString()=="123456")
             {
-                val intent =Intent(this@SigninActivity,ProfileActivity::class.java)
+                val intent: Intent =Intent(this@LoginActivity,ProfileActivity::class.java)
                 startActivity(intent)
+           }
+            else {
+                val dialog = AlertDialog.Builder(this)
+                dialog.apply{
+                    setTitle("Account khong hop le")
+                    dialog.setPositiveButton("OK"){_,_->}
+                }
+                dialog.show()
             }
 
         }
--- week2/app/src/main/java/com/example/week1/MainActivity.kt
+++ week2/app/src/main/java/com/example/week1/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.ActionBar
 
 
 class MainActivity : AppCompatActivity() {
